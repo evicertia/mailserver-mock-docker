ddd38d9f1a345175a9956f1d65ecdd6051f7097a
mmunoz <mmunoz@evicertia.com>
2020-06-26 18:20:28 +0200

    Change inetd entrypoint, to avoid removing other added services after restarting the container.

--- inetd-entrypoint.sh
+++ inetd-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo -e 'imap    stream    tcp    nowait    root    /usr/sbin/imapd imapd' > /etc/inetd.conf
-
+[ -z "$(grep 'imap' /etc/inetd.conf)" ] && echo -e 'imap    stream    tcp    nowait    root    /usr/sbin/imapd imapd' >> /etc/inetd.conf
 # User for IMAP mailbox
 adduser --disabled-password herma || :
 echo "$IMAPUSER:$IMAPPASS" | chpasswd
